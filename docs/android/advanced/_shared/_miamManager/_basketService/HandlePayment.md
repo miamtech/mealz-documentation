d29f055b503ca8693a83afcf420f973f34337e4e
Thibault Corbrion <thibault.corbrion@miam.tech>
2024-05-28 11:41:51 +0200

    add mdx supplier init doc

---
+++ docs/android/advanced/_shared/_miamManager/_basketService/HandlePayment.md
@@ -0,0 +1,5 @@
+```kotlin
+import com.miam.core.Mealz
+
+Mealz.basket.handlePayment(totalAmount: <#T##Double#>)
+```
--- docs/android/advanced/_shared/_miamManager/_basketService/index.js
+++ docs/android/advanced/_shared/_miamManager/_basketService/index.js
@@ -3,3 +3,5 @@ export { default as InitializeBasket } from './InitializeBasket.md';
 export { default as NewBasketService } from './NewBasketService.md';
 export { default as OnBasketUpdate } from './OnBasketUpdate.md';
 export { default as UpdateBasketFromExternalSource } from './UpdateBasketFromExternalSource.md';
+export { default as HandlePayment } from './HandlePayment.md';
+
